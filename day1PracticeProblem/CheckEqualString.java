260630f318d6ae58339b958a94bd11e15247deb6
AryaBhuwan-98 <117825039+AryaBhuwan-98@users.noreply.github.com>
2022-11-10 06:23:05 -0800

    Add files via upload

---
+++ day1PracticeProblem/CheckEqualString.java
@@ -0,0 +1,12 @@
+class CheckEqualString {
+    public static void main(String[] args) {
+        String a = "bhuwan";
+        String b = "bhuwana";
+
+        if (a == b) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
---
+++ day1PracticeProblem/PrimitiveDefaultValue.java
@@ -0,0 +1,12 @@
+public class PrimitiveDefaultValue{
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println(a);
+        float b = 0f;
+        System.out.println(b);
+        char c = 'a';
+        System.out.println(c);
+        boolean d = false;
+        System.out.println(d);
+    }
+}
