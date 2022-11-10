650d94717867b82ee0b1f881528d0d730b128a99
AryaBhuwan-98 <117825039+AryaBhuwan-98@users.noreply.github.com>
2022-11-10 05:36:27 -0800

    Add files via upload

---
+++ DisplayAMessage.java
@@ -0,0 +1,11 @@
+package Day1Problems;
+public class DisplayAMessage {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String a = "Hello World";
+        System.out.println(a);
+    }
+}
+
